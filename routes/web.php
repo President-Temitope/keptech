c1f9da351d5b8113f39b5ff316d0294e2fa1ba2b
halphaman <abdulazeezyisau@gmIal.com>
2021-03-01 08:19:08 +0100

    quick bit

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@index');
+Route::get('/', 'PageController@index');
 
 
 Route::group(['prefix' => 'admin'], function () {
