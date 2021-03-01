41b75f163cb7c724a4609f881e80e25b15c1b018
halphaman <abdulazeezyisau@gmIal.com>
2021-03-01 08:10:44 +0100

    quick bit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PagesController extends Controller
 {
-    public function index(){
-return view('index');
+    public function index()
+    {
+        return view('index');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,53 +2,36 @@
 @section('content')
     <div class="bg-light border rounded border-light hero-technology jumbotron py-5 px-4"
          style="height: 600px;background: url(&quot;assets/img/BKG%209.png&quot;) center;max-width: 100%;">
-        <h1 class="hero-title" style="text-align: left;margin-top: 100px;margin-bottom: 20px;">Hero Technology</h1>
-        <p style="text-align: left;"><a class="btn btn-primary btn-lg hero-button" role="button" href="#">Get In
-                Touch</a></p>
+        <h1 class="hero-title" style="text-align: left;margin-top: 200px;margin-bottom: 20px;margin-left: 20px;">Hero
+            Technology</h1>
+        <p style="text-align: left;"><a class="btn btn-primary btn-lg hero-button" role="button" href="#"
+                                        style="margin-left: 20px;">Get In Touch</a></p>
     </div>
     <div class="row clearmargin clearpadding row-image-txt"
          style="background: #F2F3F4;padding-top: 150px;max-width: 100%;">
         <div class="col-xs-12 col-sm-6 col-md-6 clearmargin clearpadding col-sm-push-6"
              style="height: 400px;background: #F2F3F4;">
             <div></div>
-            <h1 style="color: #000000;margin-left: 20px;">About Us</h1>
-            <hr style="background: #2aa3ff;height: 5px;width: 100px;">
-            <p style="color: rgb(0,0,0);font-family: Aleo, serif;font-size: 18px;font-weight: normal;margin-left: 20px;">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel lorem a lectus finibus bibendum ut
-                sagittis diam. Nunc egestas nulla in elit congue, id pellentesque ex semper. Aenean euismod tellus
-                tincidunt massa eleifend, a efficitur lectus ultricies. Aliquam bibendum et leo quis consectetur.
-                Integer consequat consequat mauris, id porttitor sem feugiat eu. Proin ac neque eu nisi viverra mattis.
-                Curabitur molestie lectus ac sollicitudin faucibus. Duis congue ipsum eget justo egestas blandit. bb</p>
+            <h1 style="color: #000000;margin-left: 50px;">About Us</h1>
+            <hr style="background: #2aa3ff;height: 5px;width: 100px;margin-left: 50px;">
+            <p style="color: rgb(0,0,0);font-family: Aleo, serif;font-size: 18px;font-weight: normal;margin-left: 50px;">
+                We are a talented team of data analysts, developers, IT solutions experts from different disciplines and
+                background brought together by a common mission: increase profitability in business by providing
+                innovative and reliable technological solutions.<br></p>
             <button class="btn btn-light btn-lg" type="button"
-                    style="background: #58CCED;border-radius: 20px;border-width: 100px;border-style: none;margin-left: 20px;">
+                    style="background: #58CCED;border-radius: 20px;border-width: 100px;border-style: none;margin-left: 50px;">
                 READ MORE
             </button>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-6 col-sm-pull-6" style="padding: 20px;background: #F2F3F4;"><img
-                src="assets/img/about-us-300x293.png"></div>
+                src="assets/img/about-us-300x293.png" style="margin-left: 250px"></div>
     </div>
     <div class="container">
+        <h3 align="center">Services</h3>
         <div class="row">
+
             <div class="col-md-3">
-                <div class="card shadow" style="width: 20rem;">
-                    <div class="inner">
-                        <img src="assets/img/sl-texture_verte.jpg" class="card-img-top img-responsive" alt="..."></div>
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Business Analysis</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card shadow" style="width: 20rem;">
-                    <div class="inner">
-                        <img src="assets/img/sl-texture_verte.jpg" class="card-img-top img-responsive" alt="..."></div>
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Business Analysis</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="card shadow" style="width: 20rem;">
+                <div class="card shadow" style="width: 20rem; height: 500px">
                     <div class="inner">
                         <img src="assets/img/sl-texture_verte.jpg" class="card-img-top img-responsive" alt="..."></div>
                     <div class="card-body text-center">
@@ -66,7 +49,7 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+       {{-- <div class="row" style="margin-top: 50px;">
             <div class="col-md-3">
                 <div class="card shadow" style="width: 20rem;">
                     <div class="inner">
@@ -94,13 +77,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
-                <button class="btn btn-primary" type="button"
-                        style="width: 200px;height: 45px;margin-top: 150px;margin-left: 50px;background: #58CCED;border-radius: 20px;color: rgb(3,10,71);">
-                    ALL SERVICES
-                </button>
-            </div>
-        </div>
+
+        </div>--}}
     </div>
     <div class="container" style="background: #072F5F;height: 89px;width: 2000px;max-width: 100%;">
         <div class="row">
